a8460e4431ef8561d98f4653335e3676414f1586
Tiago Soares <tsgo27@hotmail.com>
2025-05-07 01:04:48 -0300

    Update icon sidebar menu

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -9,7 +9,7 @@
 </head>
 
 <body>
-    <aside class="flex flex-col w-64 h-screen px-5 py-8 overflow-y-auto bg-white border-r white:bg-gray-900 white:border-gray-700">
+    <aside class="flex flex-col w-64 h-screen px-5 py-8 overflow-y-auto bg-white border-r">
         <a href="/dashboard" class="flex items-center gap-x-2">
             <img class="w-auto h-7" src="/img/logo-colegio.png" width="20" alt="">
             <p class="text-base font-medium">School Library</p>
@@ -17,7 +17,7 @@
 
         <div class="flex flex-col justify-between flex-1 mt-6">
             <nav class="-mx-3 space-y-3">
-                <!-- ícone HOME -->
+                <!-- Home -->
                 <a class="flex items-center px-3 py-2 transition hover:bg-gray-100 rounded-lg text-black" href="/dashboard">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6 text-black">
                         <path d="M11.47 3.841a.75.75 0 0 1 1.06 0l8.69 8.69a.75.75 0 1 0 1.06-1.061l-8.689-8.69a2.25 2.25 0 0 0-3.182 0l-8.69 8.69a.75.75 0 1 0 1.061 1.06l8.69-8.689Z" />
@@ -26,112 +26,84 @@
                     <span class="ml-2 text-sm font-medium">Home</span>
                 </a>
 
-                <!-- Primeiro submenu itens de cadadastros -->
+                <!-- Cadastros com submenu -->
                 <div x-data="{ open: false }" class="space-y-1">
                     <button @click="open = !open"
-                        class="flex items-center justify-between w-full px-3 py-2 text-sm font-medium text-black hover:text-black transition rounded-lg hover:bg-gray-100 white:hover:bg-gray-800">
+                        class="flex items-center justify-between w-full px-3 py-2 text-sm font-medium text-black hover:text-black transition rounded-lg hover:bg-gray-100">
                         <div class="flex items-center gap-x-2">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
-                            <path fill-rule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM12.75 9a.75.75 0 0 0-1.5 0v2.25H9a.75.75 0 0 0 0 1.5h2.25V15a.75.75 0 0 0 1.5 0v-2.25H15a.75.75 0 0 0 0-1.5h-2.25V9Z" clip-rule="evenodd" />
-                        </svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+                                <path fill-rule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM12.75 9a.75.75 0 0 0-1.5 0v2.25H9a.75.75 0 0 0 0 1.5h2.25V15a.75.75 0 0 0 1.5 0v-2.25H15a.75.75 0 0 0 0-1.5h-2.25V9Z" clip-rule="evenodd" />
+                            </svg>
+
                             <span>Cadastros</span>
                         </div>
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24" class="w-5 h-5">
                             <path fill-rule="evenodd" d="M12.53 16.28a.75.75 0 0 1-1.06 0l-7.5-7.5a.75.75 0 0 1 1.06-1.06L12 14.69l6.97-6.97a.75.75 0 1 1 1.06 1.06l-7.5 7.5Z" clip-rule="evenodd" />
                         </svg>
                     </button>
 
                     <div x-show="open" x-collapse class="pl-6 space-y-2">
-                        <a class="flex items-center px-3 py-2 dark:text-black transition-colors duration-300 transform rounded-lg dark:dark:text-black-200 
-                        hover:bg-gray-100 dark:hover:dark:text-black-200 hover:dark:text-black-700" href="/emprestimos">
-
-                            <span class="text-sm font-medium">Empréstimos</span>
-                        </a>
-                        <a class="flex items-center px-3 py-2 dark:text-black transition-colors duration-300 transform rounded-lg dark:dark:text-black-200 
-                        hover:bg-gray-100 dark:hover:dark:text-black-200 hover:dark:text-black-700" href="#">
-
-                            <span class="text-sm font-medium">Reservas</span>
+                        <!-- Empréstimos -->
+                        <a class="flex items-center px-3 py-2 hover:bg-gray-100 rounded-lg" href="/emprestimos">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+                                <path fill-rule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25Zm4.28 10.28a.75.75 0 0 0 0-1.06l-3-3a.75.75 0 1 0-1.06 1.06l1.72 1.72H8.25a.75.75 0 0 0 0 1.5h5.69l-1.72 1.72a.75.75 0 1 0 1.06 1.06l3-3Z" clip-rule="evenodd" />
+                            </svg>
+                            <span class="ml-2 text-sm font-medium">Empréstimos</span>
                         </a>
 
-                        <a class="flex items-center px-3 py-2 dark:text-black transition-colors duration-300 transform rounded-lg dark:dark:text-black-200 
-                        hover:bg-gray-100 dark:hover:dark:text-black-200 hover:dark:text-black-700" href="#">
-
-                            <span class="text-sm font-medium">Obras</span>
+                        <!-- Reservas -->
+                        <a class="flex items-center px-3 py-2 hover:bg-gray-100 rounded-lg" href="#">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+                                <path fill-rule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM12.75 6a.75.75 0 0 0-1.5 0v6c0 .414.336.75.75.75h4.5a.75.75 0 0 0 0-1.5h-3.75V6Z" clip-rule="evenodd" />
+                            </svg>
+                            <span class="ml-2 text-sm font-medium">Reservas</span>
                         </a>
 
-                        <a class="flex items-center px-3 py-2 dark:text-black transition-colors duration-300 transform rounded-lg dark:dark:text-black-200 
-                        hover:bg-gray-100 dark:hover:dark:text-black-200 hover:dark:text-black-700" href="/autores">
-
-                            <span class="text-sm font-medium">Autores</span>
+                        <!-- Obras -->
+                        <a class="flex items-center px-3 py-2 hover:bg-gray-100 rounded-lg" href="#">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+                                <path d="M11.25 4.533A9.707 9.707 0 0 0 6 3a9.735 9.735 0 0 0-3.25.555.75.75 0 0 0-.5.707v14.25a.75.75 0 0 0 1 .707A8.237 8.237 0 0 1 6 18.75c1.995 0 3.823.707 5.25 1.886V4.533ZM12.75 20.636A8.214 8.214 0 0 1 18 18.75c.966 0 1.89.166 2.75.47a.75.75 0 0 0 1-.708V4.262a.75.75 0 0 0-.5-.707A9.735 9.735 0 0 0 18 3a9.707 9.707 0 0 0-5.25 1.533v16.103Z" />
+                            </svg>
+                            <span class="ml-2 text-sm font-medium">Obras</span>
                         </a>
 
-                        <a class="flex items-center px-3 py-2 dark:text-black transition-colors duration-300 transform rounded-lg dark:dark:text-black-200 
-                        hover:bg-gray-100 dark:hover:dark:text-black-200 hover:dark:text-black-700" href="#">
-
-                            <span class="text-sm font-medium">Acervos</span>
+                        <!-- Autores -->
+                        <a class="flex items-center px-3 py-2 hover:bg-gray-100 rounded-lg" href="/autores">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+                                <path fill-rule="evenodd" d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z" clip-rule="evenodd" />
+                            </svg>
+                            <span class="ml-2 text-sm font-medium">Autores</span>
                         </a>
 
-                        <a class="flex items-center px-3 py-2 dark:text-black transition-colors duration-300 transform rounded-lg dark:dark:text-black-200 
-                        hover:bg-gray-100 dark:hover:dark:text-black-200 hover:dark:text-black-700" href="#">
-
-                            <span class="text-sm font-medium">Gêneros</span>
+                        <!-- Acervos -->
+                        <a class="flex items-center px-3 py-2 hover:bg-gray-100 rounded-lg" href="#">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+                                <path d="M5.566 4.657A4.505 4.505 0 0 1 6.75 4.5h10.5c.41 0 .806.055 1.183.157A3 3 0 0 0 15.75 3h-7.5a3 3 0 0 0-2.684 1.657ZM2.25 12a3 3 0 0 1 3-3h13.5a3 3 0 0 1 3 3v6a3 3 0 0 1-3 3H5.25a3 3 0 0 1-3-3v-6ZM5.25 7.5c-.41 0-.806.055-1.184.157A3 3 0 0 1 6.75 6h10.5a3 3 0 0 1 2.683 1.657A4.505 4.505 0 0 0 18.75 7.5H5.25Z" />
+                            </svg>
+                            <span class="ml-2 text-sm font-medium">Acervos</span>
                         </a>
-                        <a class="flex items-center px-3 py-2 dark:text-black transition-colors duration-300 transform rounded-lg dark:dark:text-black-200 
-                        hover:bg-gray-100 dark:hover:dark:text-black-200 hover:dark:text-black-700" href="#">
 
-                            <span class="text-sm font-medium">Alunos</span>
+                        <!-- Gêneros -->
+                        <a class="flex items-center px-3 py-2 hover:bg-gray-100 rounded-lg" href="#">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+                                <path fill-rule="evenodd" d="M3 6a3 3 0 0 1 3-3h2.25a3 3 0 0 1 3 3v2.25a3 3 0 0 1-3 3H6a3 3 0 0 1-3-3V6Zm9.75 0a3 3 0 0 1 3-3H18a3 3 0 0 1 3 3v2.25a3 3 0 0 1-3 3h-2.25a3 3 0 0 1-3-3V6ZM3 15.75a3 3 0 0 1 3-3h2.25a3 3 0 0 1 3 3V18a3 3 0 0 1-3 3H6a3 3 0 0 1-3-3v-2.25Zm9.75 0a3 3 0 0 1 3-3H18a3 3 0 0 1 3 3V18a3 3 0 0 1-3 3h-2.25a3 3 0 0 1-3-3v-2.25Z" clip-rule="evenodd" />
+                            </svg>
+                            <span class="ml-2 text-sm font-medium">Gêneros</span>
                         </a>
-                    </div>
 
-                </div>
-
-                <!-- Segundo submenu itens Inativos-->
-                <div x-data="{ open: false }" class="space-y-1">
-                    <button @click="open = !open"
-                        class="flex items-center justify-between w-full px-3 py-2 text-sm font-medium text-black hover:text-black transition rounded-lg hover:bg-gray-100 white:hover:bg-gray-800">
-                        <div class="flex items-center gap-x-2">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
-                            <path fill-rule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM12.75 6a.75.75 0 0 0-1.5 0v6c0 .414.336.75.75.75h4.5a.75.75 0 0 0 0-1.5h-3.75V6Z" clip-rule="evenodd" />
-                        </svg>
-
-                            <span>Itens Inativos</span>
-                        </div>
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
-                            <path fill-rule="evenodd" d="M12.53 16.28a.75.75 0 0 1-1.06 0l-7.5-7.5a.75.75 0 0 1 1.06-1.06L12 14.69l6.97-6.97a.75.75 0 1 1 1.06 1.06l-7.5 7.5Z" clip-rule="evenodd" />
-                        </svg>
-
-                    </button>
-
-                    <div x-show="open" x-collapse class="pl-6 space-y-2">
-                        <a class="flex items-center px-3 py-2 dark:text-black transition-colors duration-300 transform rounded-lg dark:dark:text-black-200 
-                        hover:bg-gray-100 dark:hover:dark:text-black-200 hover:dark:text-black-700" href="#">
-
-                            <span class="text-sm font-medium">Obras</span>
-                        </a>
-                        <a class="flex items-center px-3 py-2 dark:text-black transition-colors duration-300 transform rounded-lg dark:dark:text-black-200 
-                        hover:bg-gray-100 dark:hover:dark:text-black-200 hover:dark:text-black-700" href="#">
-
-                            <span class="text-sm font-medium">Autores</span>
-                        </a>
-                        <a class="flex items-center px-3 py-2 dark:text-black transition-colors duration-300 transform rounded-lg dark:dark:text-black-200 
-                        hover:bg-gray-100 dark:hover:dark:text-black-200 hover:dark:text-black-700" href="#">
-
-                            <span class="text-sm font-medium">Editoras</span>
-                        </a>
-                        <a class="flex items-center px-3 py-2 dark:text-black transition-colors duration-300 transform rounded-lg dark:dark:text-black-200 
-                        hover:bg-gray-100 dark:hover:dark:text-black-200 hover:dark:text-black-700" href="#">
-
-                            <span class="text-sm font-medium">Acervos</span>
-                        </a>
-                        <a class="flex items-center px-3 py-2 dark:text-black transition-colors duration-300 transform rounded-lg dark:dark:text-black-200 
-                        hover:bg-gray-100 dark:hover:dark:text-black-200 hover:dark:text-black-700" href="#">
-
-                            <span class="text-sm font-medium">Alunos</span>
+                        <!-- Alunos -->
+                        <a class="flex items-center px-3 py-2 hover:bg-gray-100 rounded-lg" href="#">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+                                <path d="M4.5 6.375a4.125 4.125 0 1 1 8.25 0 4.125 4.125 0 0 1-8.25 0ZM14.25 8.625a3.375 3.375 0 1 1 6.75 0 3.375 3.375 0 0 1-6.75 0ZM1.5 19.125a7.125 7.125 0 0 1 14.25 0v.003l-.001.119a.75.75 0 0 1-.363.63 13.067 13.067 0 0 1-6.761 1.873c-2.472 0-4.786-.684-6.76-1.873a.75.75 0 0 1-.364-.63l-.001-.122ZM17.25 19.128l-.001.144a2.25 2.25 0 0 1-.233.96 10.088 10.088 0 0 0 5.06-1.01.75.75 0 0 0 .42-.643 4.875 4.875 0 0 0-6.957-4.611 8.586 8.586 0 0 1 1.71 5.157v.003Z" />
+                            </svg>
+                            <span class="ml-2 text-sm font-medium">Alunos</span>
                         </a>
                     </div>
                 </div>
             </nav>
         </div>
     </aside>
+
 </body>
+
 </html>
